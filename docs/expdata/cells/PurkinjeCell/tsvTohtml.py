8b22e093ac8d725d4ad7624f5526732d8b4968eb
lungsi <neuralgraphs@gmail.com>
2019-11-20 12:10:08 +0000

    corrected tsvTohtml

--- docs/expdata/cells/PurkinjeCell/tsvTohtml.py
+++ docs/expdata/cells/PurkinjeCell/tsvTohtml.py
@@ -3,11 +3,13 @@
 #                        HOW TO USE THIS SCRIPT
 # python tsvTohtml.py "desired_data.txt" "Some dummy title"
 ##############################################################################
-#from pdb import set_trace as breakpoint
+from pdb import set_trace as breakpoint
 import os
 from sys import argv
 import csv
-from json2html import json2html
+from html import escape
+from collections import OrderedDict
+#from json2html import json2html
 from bs4 import BeautifulSoup as bs
 
 thispythonfile, filename, title_content = argv
@@ -24,9 +26,34 @@ filepath = os.getcwd()+ os.sep + "expdata" + os.sep + "cells" + os.sep + \
 dlist = []
 with open(filepath, newline="") as f:
     data = csv.DictReader(f, delimiter="\t")
-    for row in data:
-        dlist.append(dict(row))
+    for row in data: # Note: Python >= 3.8 row is a dict
+        #dlist.append(dict(row))
+        dlist.append( OrderedDict(row) )
 os.chdir(pwd) # return to ~/cerebunitdata/docs/expdata/cells/PurkinjeCell
+#print( len(dlist) )
+#breakpoint()
+######################
+def html_table(ddict): # dlist[0]
+    "https://stackoverflow.com/questions/44320329/converting-csv-to-html-table-in-python"
+    clmn_names = []
+    for row in ddict:
+        for name in row.keys():
+            if name not in clmn_names:
+                clmn_names.append( name )
+    html_lines = []
+    html_lines.append("<table>\n")
+    html_lines.append("  <tr>\n")
+    for name in clmn_names:
+        html_lines.append("   <th>{}</th>\n".format(escape(name)))
+    html_lines.append("  </tr>\n")
+    for row in ddict:
+        html_lines.append("  <tr>\n")
+        for name in clmn_names:
+            value = row.get(name, "")
+            html_lines.append("   <td>{}</td>\n".format(escape(value)))
+        html_lines.append("  </tr>\n")
+    html_lines.append("</table>")
+    return "".join(html_lines)
 
 ######################### CREATE THE HTML CONTAINER  ##########################
 structure = """
@@ -44,7 +71,8 @@ title.insert(0, title_content)
 
 ######################### CONVERT JSON DATA TO HTML  ##########################
 #htmltable = json2html.convert(json = jsondata)
-htmltable = json2html.convert(json = dlist[0])
+#htmltable = json2html.convert(json = dlist[0])
+htmltable = html_table( dlist )
 extrasoup = bs(htmltable, "html.parser")
 
 ###################### INSERT TABLE INTO HTML CONTAINER #######################
@@ -55,4 +83,3 @@ body.insert(0, extrasoup)
 savefile = pwd + os.sep + "rawhtmls" + os.sep + filename.replace(".txt",".html")
 html = open( savefile, "w", encoding="utf-8" )
 html.write( str(soup) )
-
